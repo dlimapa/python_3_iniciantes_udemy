7d82421cb0d78dd85d63e561f9bccb3b12d15bc8
Daniel de Lima <dlimapa@gmail.com>
2021-12-23 11:03:13 -0300

    logico e de comparaçao

--- curso.py
+++ curso.py
@@ -28,3 +28,38 @@ print(23 % 5)
 
 print(23 // 5)
 
+#operadores logicos
+
+var = True
+
+print(type(var))
+
+print(not var)
+
+print(var)
+
+v1 = True
+v2 = False
+
+print(v1 and v2)
+
+print(v1 or v2)
+
+v1 = False
+v2 = False
+
+print(v1 or v2)
+
+#comparação
+
+print(2 > 3)
+
+print(2 <= 3)
+
+print(2 == 3)
+
+print(3 == 3)
+
+print(2 != 3)
+
+print(3 != 3)
